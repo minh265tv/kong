c6c0efb4820b6497cb213e492b06dc3946954011
Minh NQ <minh.tonn@gmail.com>
2021-08-02 09:25:13 +0700

    update config

--- kus.js
+++ kus.js
@@ -1,13 +1,13 @@
 
 module.exports = [
     {
-        "username": "admin",
+        "username": "minhnq",
         "email": "admin@gviet.vn",
         "firstName": "Nguyen",
         "lastName": "Admin",
         "node_id": "http://kong:3001",
         "admin": true,
         "active": true,
-        "password": "12345678"
+        "password": "123456@aA"
     }
 ];
